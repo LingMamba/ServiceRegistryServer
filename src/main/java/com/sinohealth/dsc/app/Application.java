ada0696aafe4158788c610c83e151f78315d0de1
LingMamaba <61710670@qq.com>
2018-02-01 11:39:14 +0800

    test

--- src/main/java/com/sinohealth/dsc/app/Application.java
+++ src/main/java/com/sinohealth/dsc/app/Application.java
@@ -9,7 +9,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);//111222
+		SpringApplication.run(Application.class, args);//111222333
 	}
 
 }
